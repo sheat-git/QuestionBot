7d2a1c1254f275cae2b7cf715e57ba8ce459dee5
sheat <79030436+sheat-git@users.noreply.github.com>
2021-11-15 21:55:40 +0900

    Add files via upload

--- QuestionBot.py
+++ QuestionBot.py
@@ -6,7 +6,7 @@ import io
 TOKEN = os.environ['QUESTIONBOT_TOKEN']
 Q_CHANNEL_ID = os.environ['QUESTION_CHANNEL_ID']
 Q_CHANNEL = None
-template = f'__以下の内容を <#{Q_CHANNEL_ID}> に送信します。\nよろしければ :o: を押してください。__'
+template = f'__以下の内容を <#{Q_CHANNEL_ID}> に送信します。\nよろしければ :o: を押してください。__\n\n'
 client = discord.Client()
 
 
@@ -32,7 +32,7 @@ async def on_message(message: discord.Message):
                 files.append(discord.File(io.BytesIO(await resp.read()), f.filename))
     if not files:
         files = None
-    msg = await message.channel.send(f'{template}\n{message.content}', files=files)
+    msg = await message.channel.send(template+message.content, files=files)
     await msg.add_reaction('⭕')
 
 
@@ -60,7 +60,7 @@ async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
                 files.append(discord.File(io.BytesIO(await resp.read()), f.filename))
     if not files:
         files = None
-    await Q_CHANNEL.send(message.content.replace(template+'\n', ''), files=files)
+    await Q_CHANNEL.send(message.content.replace(template, ''), files=files)
     await channel.send(f'<#{Q_CHANNEL_ID}> に送信完了しました。')
 
 
